1bf96a4d29ff380f0c9b3d3fe2a640d380e5008b
Andersson Goncalves <anderssonsilva793@gmail.com>
2025-07-20 19:34:25 -0300

    fix: api base url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://fume-artes-api.onrender.com:3000"
+    ? "https://fume-artes-api.onrender.com"
     : "http://127.0.0.1:3000",
   timeout: 15000,
 });
