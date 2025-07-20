6aa1e0a2a65dce4792a72899040b3b5c32ecb482
Andersson Goncalves <anderssonsilva793@gmail.com>
2025-07-20 11:51:25 -0300

    feat: change base url based on dev or prod

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://127.0.0.1:3000",
+  baseURL: import.meta.env.PROD
+    ? "https://fume-artes-api.onrender.com:3000"
+    : "http://127.0.0.1:3000",
   timeout: 15000,
-  headers: {
-    //"Access-Control-Allow-Origin": "*",
-  },
 });
