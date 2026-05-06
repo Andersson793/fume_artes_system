5a4c85fbf783e498ab146628c742bc2fff0ab9f7
Andersson Goncalves <anderssonsilva793@gmail.com>
2026-05-06 11:35:31 -0300

    feat: fetchData function

--- src/axios.js
+++ src/axios.js
@@ -6,3 +6,31 @@ export const instance = axios.create({
     : "http://127.0.0.1:3000",
   timeout: 55000,
 });
+
+export function fetchData(path, body) {
+  
+  const baseURL = import.meta.env.PROD ? "https://fume-artes-api.onrender.com": "http://127.0.0.1:3000";
+  const URL = baseURL.concat(path);
+  const method = "GET"
+
+  let response;
+
+  try{
+    response = fetch(URL, {
+      method: method,
+      body: body,
+      headers: {
+        "Authorization": sessionStorage.get("token")
+      }
+    })
+
+    if(!response.ok){
+      console.log(response.status)
+    }
+    
+  }catch (error) {
+    console.log(error)
+  }
+  
+  return response.json()
+}
